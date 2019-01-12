345cc302aea5e7ee72b5f3962e6ee79dba630ad7
James Cole <mail@ofjamescole.com>
2019-01-12 14:57:19 -0500

    Update project info container sections

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,20 +1,34 @@
 import styled from 'styled-components'
 
-import { media, shevy, themeValue } from './theme'
-import { P } from './Typography'
+import { media, shevy } from './theme'
+import { P, WYSIWYG } from './Typography'
 
-export const ProjectIntro = styled(P)`
-  font-size: ${shevy.h4.fontSize};
-  line-height: ${shevy.h4.lineHeight};
-  margin: 0;
-
-  ${media.medium`
-    flex: 1;
-    font-size: ${shevy.h2.fontSize};
-    line-height: ${shevy.h2.lineHeight};
-  `};
-`
+export const ProjectIntro = styled.div``
 ProjectIntro.displayName = 'ProjectIntro'
+
+export const ProjectIntroLead = styled.div`
+  ${media.medium`flex: 1;`};
+
+  p {
+    font-size: ${shevy.h4.fontSize};
+    line-height: ${shevy.h4.lineHeight};
+    margin-bottom: ${shevy.baseSpacing(1)};
+
+    ${media.medium`
+      font-size: ${shevy.h2.fontSize};
+      line-height: ${shevy.h2.lineHeight};
+      margin-bottom: ${shevy.baseSpacing(3)};
+    `};
+  }
+`
+ProjectIntroLead.displayName = 'ProjectIntroLead'
+
+export const ProjectIntroInfo = styled(WYSIWYG)`
+  p {
+    ${media.medium`font-size: ${shevy.h4.fontSize};`}
+  }
+`
+ProjectIntroInfo.displayName = 'ProjectIntroInfo'
 
 export const ProjectHeader = styled.div`
   display: flex;
@@ -30,9 +44,13 @@ export const ProjectHeader = styled.div`
 ProjectHeader.displayName = 'ProjectHeader'
 
 export const ProjectMetaContainer = styled.dl`
+  display: flex;
+  justify-content: space-between;
+
   ${media.medium`
-    margin: 0 ${shevy.baseSpacing(2)} 0 0;
-    flex-basis: ${shevy.baseSpacing(7)};
+    display: block;
+    margin: 0 ${shevy.baseSpacing(4)} 0 0;
+    flex-basis: ${shevy.baseSpacing(24)};
     order: -1;
   `}
 `
